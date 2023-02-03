dbb90424d0403a770a7a03b7a92425949babd14c
MJ-Curtido <manu17pc@gmail.com>
2023-02-03 10:58:01 +0100

    Arreglado lo de uuid

--- src/app/modelo/curso.ts
+++ src/app/modelo/curso.ts
@@ -1,12 +1,12 @@
-//import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 export class Curso {
-    //private id: String;
+    private id: String;
     private nombre:String;
     private valoracion:Number;
 
     constructor(nombre:String, valoracion: Number) {
-        //this.id = uuidv4();
+        this.id = uuidv4();
         this.nombre = nombre;
         this.valoracion = valoracion;
     }
